b97e2f4868f318251785c1f67e1f39088558e4e4
Raj Nakarja <raj@siliconwitchery.com>
2023-03-28 23:51:35 +0200

    Initial Commit

---
+++ a eye/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  a eye
+//
+//  Created by Raj Nakarja on 2023-03-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ a eye/a_eyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  a_eyeApp.swift
+//  a eye
+//
+//  Created by Raj Nakarja on 2023-03-28.
+//
+
+import SwiftUI
+
+@main
+struct a_eyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
