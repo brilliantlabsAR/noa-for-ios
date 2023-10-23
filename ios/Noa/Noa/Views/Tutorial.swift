8bc8181b2d7c08b5baa0bbabaf15b549370dc8db
Bart Trzynadlowski <bart.trzy@gmail.com>
2023-10-23 09:48:27 -0700

    Updated tutorial to match latest Figma

--- ios/Noa/Noa/Views/Tutorial.swift
+++ ios/Noa/Noa/Views/Tutorial.swift
@@ -10,10 +10,9 @@ import UIKit
 func displayTutorialInChatWindow(chatMessageStore: ChatMessageStore) async throws {
     let messages: [(pause: Float, image: UIImage?, text: String)] = [
         ( pause: 2, image: nil, text: "Hi, I'm Noa. Let's show you around 🙂" ),
-        ( pause: 5, image: UIImage(named: "Tutorial_2"), text: "Hold either of the touch pads and speak.\n\nAsk me any question, and I'll respond directly on your Monocle." ),
+        ( pause: 5, image: UIImage(named: "Tutorial_2"), text: "Tap either of the touch pads and speak.\n\nAsk me any question, and I'll respond directly on your Monocle." ),
         ( pause: 5, image: UIImage(named: "Tutorial_3"), text: "I can also translate whatever I hear into English.\n\nToggle the translator mode from the menu like so." ),
         ( pause: 5, image: UIImage(named: "Tutorial_4"), text: "Did you know that I'm a fantastic artist? Tap then hold, and Monocle will take a picture before listening.\n\nAsk me how to change the image, and I'll return back a new image right here in the chat." ),
-        ( pause: 5, image: UIImage(named: "Tutorial_5"), text: "To get started, you'll need an OpenAI API key. To create one, visit:\n\n[https://platform.openai.com](https://platform.openai.com)\n\nAdditionally, to use the AI art feature, you'll need a Stability AI key. Get it here:\n\n[https://platform.stability.ai](https://platform.stability.ai)" ),
         ( pause: 0, image: nil, text: "Looks like you're all set!\n\nGo ahead. Ask me anything you'd like ☺️" )
     ]
 
